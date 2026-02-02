146a5b292966b8f67e620319d48ed1cc2ab4ac19
DyegoMFernandes <dffmoreira@outlook.com>
2026-02-02 19:31:34 -0300

    TesZte

--- Program.cs
+++ Program.cs
@@ -6,3 +6,19 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Average C# Enjoyer");
+
+
+// Linha final
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Average C# Enjoyer");
+
+// Teste
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Average C# Enjoyer");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Average C# Enjoyer");
